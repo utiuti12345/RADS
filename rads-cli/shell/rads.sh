12c7d9060fb42280c1aafb89bfddd5bc4371b1db
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2020-02-02 21:55:45 +0900

    Modify RADS cli cronjob

--- rads-cli/shell/rads.sh
+++ rads-cli/shell/rads.sh
@@ -3,7 +3,7 @@
 # 閏年は考慮していないし、31日に実行するとだめになる
 CURRENT_MONTH=`date +"%m"`
 DAY=30
-if [ $CURRENT_MONTH = 02 ]; then
+if [ $CURRENT_MONTH = 03 ]; then
     DAY = 28
 fi
 
@@ -14,3 +14,5 @@ HOST=$1
 ROSTERNAME=$2
 
 ./rads-cli -y $YEAR -m $MONTH -f $ROSTERNAME -H $HOST
+
+wget --post-data='{ "Title":"勤務表","Message":"勤務表をドライブにコピーしました" }' --header=Content-Type:application/json $HOST/postSlack
